09cb979ff102f241bacbae73a9f6fa42db082fd0
sachin <sachncs@gmail.com>
2026-07-24 11:21:45 +0530

    [P1-P4] issue templates and PR template
    - .github/ISSUE_TEMPLATE/: bug, feature, algorithm, performance
    - .github/PULL_REQUEST_TEMPLATE.md: checklist for PRs

---
+++ reachq/research/hyper.py
@@ -0,0 +1,5 @@
+"""Reachability over hypergraphs.
+
+Extends reachability to hypergraphs where a single hyperedge connects
+an arbitrary set of vertices.
+"""
---
+++ reachq/research/temporal.py
@@ -0,0 +1,5 @@
+"""Reachability over temporal graphs.
+
+Extends reachability to graphs where edges have time stamps and
+traversal must respect temporal ordering.
+"""
